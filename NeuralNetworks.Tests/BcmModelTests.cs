d683b59cfef5291a89c484e1bb045f80978a1035
Lukasz Rabiec <lukaszrabiec@gmail.com>
2016-03-18 14:01:06 +0100

    Added partially BCM

---
+++ NeuralNetworks.Tests/BcmModelTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace NeuralNetworks.Tests
+{
+    public class BcmModelTests
+    {
+        [Fact]
+        public void FirstTrainedVectorShouldGiveMatrixSize()
+        {
+            var vector = new[] {1, 1, 0, 0, 0, 0};
+            var bcmModel = new BcmModel();
+
+            bcmModel.Train(vector);
+
+            Assert.Equal(6, bcmModel.CorrelationMatrix.GetLength(0));
+        }
+
+        [Fact]
+        public void SecondTrainedVectorShouldGiveRightMatrix()
+        {
+            const int size = 6;
+            var firstVector = new[] {1, 1, 0, 0, 0, 0};
+            var secondVector = new[] {0, 1, 0, 0, 0, 1};
+            var rightMatrix = new int[size][];
+            var bcmModel = new BcmModel();
+
+            bcmModel.Train(firstVector);
+            bcmModel.Train(secondVector);
+
+            rightMatrix[0] = new[] {1, 1, 0, 0, 0, 0};
+            rightMatrix[1] = new[] {1, 1, 0, 0, 0, 1};
+            rightMatrix[2] = new[] {0, 0, 0, 0, 0, 0};
+            rightMatrix[3] = new[] {0, 0, 0, 0, 0, 0};
+            rightMatrix[4] = new[] {0, 0, 0, 0, 0, 0};
+            rightMatrix[5] = new[] {0, 1, 0, 0, 0, 1};
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Assert.Equal(rightMatrix[i][j], bcmModel.CorrelationMatrix[i,j]);
+                }
+            }
+        }
+    }
+}
---
+++ NeuralNetworks/BcmModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetworks
+{
+    public class BcmModel
+    {
+        public int[,] CorrelationMatrix { get; private set; }
+        private int _vectorCount;
+
+        public void Train(int[] vector)
+        {
+            ValidateVector(vector);
+            
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = 0; j < vector.Length; j++)
+                {
+                    if (vector[i] * vector[j] == 1)
+                    {
+                        CorrelationMatrix[i,j] = 1;
+                    }
+                }
+            }
+
+            _vectorCount++;
+        }
+
+        private void ValidateVector(IReadOnlyCollection<int> vector)
+        {
+            switch (_vectorCount)
+            {
+                case 0:
+                {
+                    CorrelationMatrix = new int[vector.Count, vector.Count];
+                    break;
+                }
+                case 1:
+                {
+                    if (vector.Count != CorrelationMatrix.GetLength(0))
+                    {
+                        throw new ArgumentException(
+                            "The length of the vector is different than the dimension of the matrix. The correct length: " +
+                            CorrelationMatrix.GetLength(0));
+                    }
+                    break;
+                }
+                default:
+                {
+                    throw new InvalidOperationException("You can train only two vectors for one BCM!");
+                }
+            }
+        }
+    }
+}
