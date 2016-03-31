9282de4052a9a2878862471fd5b37eb9254b1056
Lukasz Rabiec <lukaszrabiec@gmail.com>
2016-03-31 15:17:15 +0200

    Added partially BamModel with Tests

---
+++ NeuralNetworks.Tests/BamModelTests.cs
@@ -0,0 +1,76 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NeuralNetworks.Tests
+{
+    public class BamModelTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly Vector<float> _image1 = Vector<float>.Build.DenseOfArray(new[] {0f, 1, 1, 1, 0, 1});
+        private readonly Vector<float> _image2 = Vector<float>.Build.DenseOfArray(new[] {1f, 0, 0, 1, 0, 0});
+        private readonly Vector<float> _name1 = Vector<float>.Build.DenseOfArray(new[] {1f, 1, 0, 0});
+        private readonly Vector<float> _name2 = Vector<float>.Build.DenseOfArray(new[] {1f, 0, 1, 0});
+
+        public BamModelTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void FirstTrainingShouldSetMatrixDimensions()
+        {
+            var bamModel = new BamModel();
+
+            bamModel.Train(_image1, _name1);
+
+            _output.WriteLine(bamModel.WeightsMatrix.ToString());
+            Assert.Equal(6, bamModel.WeightsMatrix.RowCount);
+            Assert.Equal(4, bamModel.WeightsMatrix.ColumnCount);
+        }
+
+        [Fact]
+        public void SecondTrainingShouldGiveRightWeightMatrix()
+        {
+            var bamModel = new BamModel();
+            var rightMatrix = Matrix<float>.Build.DenseOfArray(new[,]
+            {
+                {0f, -2, 2, 0},
+                {0, 2, -2, 0},
+                {0, 2, -2, 0},
+                {2, 0, 0, -2},
+                {-2, 0, 0, 2},
+                {0, 2, -2, 0}
+            });
+
+            bamModel.Train(_image1, _name1);
+            bamModel.Train(_image2, _name2);
+
+            _output.WriteLine(bamModel.WeightsMatrix.ToString());
+            Assert.Equal(rightMatrix, bamModel.WeightsMatrix);
+        }
+
+        [Fact]
+        public void TrainingByImageWithBadLengthShouldThrowArgumentException()
+        {
+            var bamModel = new BamModel();
+            var imageWithBadLength = Vector<float>.Build.DenseOfArray(new float[3]);
+
+            bamModel.Train(_image1, _name1);
+
+            Assert.Throws<ArgumentException>(() => bamModel.Train(imageWithBadLength, _name2));
+        }
+
+        [Fact]
+        public void TrainingByNameWithBadLengthShouldThrowArgumentException()
+        {
+            var bamModel = new BamModel();
+            var nameWithBadLength = Vector<float>.Build.DenseOfArray(new float[5]);
+
+            bamModel.Train(_image1, _name1);
+
+            Assert.Throws<ArgumentException>(() => bamModel.Train(_image2, nameWithBadLength));
+        }
+    }
+}
---
+++ NeuralNetworks/BamModel.cs
@@ -0,0 +1,52 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+
+namespace NeuralNetworks
+{
+    public class BamModel
+    {
+        public Matrix<float> WeightsMatrix { get; private set; }
+        private bool _isFirstTraining;
+
+
+        public void Train(Vector<float> image, Vector<float> name)
+        {
+            VerifyVectorsBeforeTraining(image, name);
+
+            for (int i = 0; i < image.Count; i++)
+            {
+                for (int j = 0; j < name.Count; j++)
+                {
+                    if ((int)image[i] == (int)name[j])
+                    {
+                        WeightsMatrix[i, j] += 1f;
+                    }
+                    else
+                    {
+                        WeightsMatrix[i, j] += -1f;
+                    }
+                }
+            }
+        }
+
+        private void VerifyVectorsBeforeTraining(Vector<float> image, Vector<float> name)
+        {
+            if (!_isFirstTraining)
+            {
+                WeightsMatrix = Matrix<float>.Build.DenseOfArray(new float[image.Count, name.Count]);
+                _isFirstTraining = true;
+            }
+            else
+            {
+                if (WeightsMatrix.RowCount != image.Count)
+                {
+                    throw new ArgumentException("Image vector has a different length than the matrix's number of rows.");
+                }
+                if (WeightsMatrix.ColumnCount != name.Count)
+                {
+                    throw new ArgumentException("Name vector has a different length than the matrix's number of columns.");
+                }
+            }
+        }
+    }
+}
