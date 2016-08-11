30a69ff605f821d0dfb86efae5577ed9ad9db725
Lukasz Rabiec <lukaszrabiec@gmail.com>
2016-08-11 17:29:56 +0200

    Ended Hopfield network

--- NeuralNetworks/Models/HopfieldNetwork.cs
+++ NeuralNetworks/Models/HopfieldNetwork.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 using NeuralNetworks.Utility;
@@ -8,6 +9,10 @@ namespace NeuralNetworks.Models
     public class HopfieldNetwork
     {
         private DataReader _dataReader;
+        private Matrix<double> _weights;
+        private Vector<double> _biases;
+        private Vector<double> _lastOutputs;
+        private Vector<double> _backendOutputs;
         private int _numberOfNeurons;
 
         public HopfieldNetwork()
@@ -15,41 +20,40 @@ namespace NeuralNetworks.Models
             _dataReader = new DataReader();
         }
 
-        public Matrix<double> Teach(string datafilePath, char separator)
+        public void TeachWithHebbsRule(string datafilePath, char separator)
         {
+            var randomizer = new Randomizer();
             var inputVectors = _dataReader.ReadHopfieldDataFromTextFile(datafilePath, separator);
-            _numberOfNeurons = CalculateNumberOfNeurons(inputVectors);
-            var weightsMatrix = CreateWeightsMatrix(inputVectors);
-
-            return weightsMatrix;
+            _numberOfNeurons = inputVectors.First().Data.Count;
+            _biases = randomizer.RandomizeBiases(inputVectors.First().Data.Count);
+            _weights = CreateWeightsMatrixWithHebbsRule(inputVectors);
         }
 
         public NeuralVector Test(NeuralVector testingVector)
         {
+            ValidateVectorLength(testingVector);
+            ValidateWeightsAndBiases();
 
+            InitializeOutputs(testingVector.Data);
 
-            return testingVector;
+            var outputVector = CalculateAllOuputs();
+
+            return new NeuralVector(outputVector);
         }
-        
-        private int CalculateNumberOfNeurons(List<NeuralVector> neuralVectors)
-        {
-            return neuralVectors.Count * neuralVectors[0].Data.Count;
-        }
 
-        private Matrix<double> CreateWeightsMatrix(List<NeuralVector> inputVectors)
+        private Matrix<double> CreateWeightsMatrixWithHebbsRule(List<NeuralVector> inputVectors)
         {
-            var vectorSize = inputVectors[0].Data.Count;
-            var weightsArray = new double[vectorSize, vectorSize];
+            var weightsArray = new double[_numberOfNeurons, _numberOfNeurons];
 
-            for (int k = 0; k < vectorSize; k++)
+            for (int k = 0; k < _numberOfNeurons; k++)
             {
-                for (int j = 0; j < vectorSize; j++)
+                for (int j = 0; j < _numberOfNeurons; j++)
                 {
                     if (k != j)
                     {
                         var sum = inputVectors.Sum(inputVector => inputVector.Data[k] * inputVector.Data[j]);
 
-                        weightsArray[k, j] = sum / vectorSize;
+                        weightsArray[k, j] = sum / _numberOfNeurons;
                     }
                 }
             }
@@ -57,17 +61,76 @@ namespace NeuralNetworks.Models
             return Matrix<double>.Build.DenseOfArray(weightsArray);
         }
 
-        private void FillMatrixDiagonalByZeros(Matrix<double> matrix)
+        private void InitializeOutputs(Vector<double> testingVectorData)
         {
-            for (int index = 0; index < matrix.ColumnCount; index++)
+            _lastOutputs = testingVectorData;
+            _backendOutputs = Vector<double>.Build.Dense(_numberOfNeurons);
+        }
+
+        private double CalculateOutputForNeuron(int neuronIndex)
+        {
+            if (!Enumerable.Range(0, _numberOfNeurons - 1).Contains(neuronIndex))
             {
-                matrix[index, index] = 0.0;
+                throw new ArgumentException("Wrong neuron index!");
+            }
+
+            var sum = 0.0;
+
+            for (int neighbour = 0; neighbour < _numberOfNeurons; neighbour++)
+            {
+                sum += _weights[neuronIndex, neighbour] * _lastOutputs[neighbour];
+            }
+
+            sum += _biases[neuronIndex];
+
+            var output = ActivationFunction(sum, neuronIndex);
+            _backendOutputs[neuronIndex] = output;
+
+            return output;
+        }
+
+        private double ActivationFunction(double activationSum, int neuronIndex)
+        {
+            if (activationSum > 0)
+            {
+                return 1.0;
+            }
+            if (activationSum < 0)
+            {
+                return -1.0;
+            }
+
+            return _lastOutputs[neuronIndex];
+        }
+
+        private Vector<double> CalculateAllOuputs()
+        {
+            var outputs = new double[_numberOfNeurons];
+
+            for (int neuron = 0; neuron < _numberOfNeurons; neuron++)
+            {
+                outputs[neuron] = CalculateOutputForNeuron(neuron);
+            }
+
+            _lastOutputs = _backendOutputs;
+
+            return Vector<double>.Build.DenseOfArray(outputs);
+        }
+
+        private void ValidateVectorLength(NeuralVector vector)
+        {
+            if (vector.Data.Count != _numberOfNeurons)
+            {
+                throw new InvalidOperationException("Weights or biases dont exist, first teach the network.");
             }
         }
 
-        private void CalculateOuputs()
+        private void ValidateWeightsAndBiases()
         {
-            //TODO:
+            if (_biases == null || _weights == null)
+            {
+                throw new InvalidOperationException("Weights or biases dont exist, first teach the network.");
+            }
         }
     }
 }
